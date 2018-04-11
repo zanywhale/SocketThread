0907304025198c67048715e490ad114e4d67836a
zanywhale <zanywhale@gmail.com>
2018-04-11 10:56:00 +0900

    Feat: Add thread

--- src/TCPServer.cc
+++ src/TCPServer.cc
@@ -11,8 +11,10 @@ namespace TCPServer{
                              
     void TCPServer::Setup(){
         this->socket_ret = socket(AF_INET, SOCK_STREAM, 0);
-        if(this->socket_ret == INVALID_SOCKET)
+        if(this->socket_ret == -1){ // Invalid socket
             std::cout << "Failed: can't create socket" << std::endl;
+            exit(1);
+        }
 
         this->saddr = {};
         this->saddr.sin_family = AF_INET;
@@ -24,18 +26,19 @@ namespace TCPServer{
             std::cerr<< "Failed: bind error" << std::endl;
             exit(1);
         }
+
         listen(this->socket_ret, 5);
         this->saddr_size = sizeof(struct sockaddr_in);
     }
 
-    void TCPServer::Run(){
-        while( (this->sock_count = accept(this->socket_ret, (struct sockaddr *)&this->caddr, (socklen_t *)&this->saddr_size)) ){
-            new std::thread(TCPServer::Handler, sock_count);
-        }
+    int TCPServer::Handler(int sockfd){
+        return 0;
     }
 
-    void TCPServer::Handler(){
-        
+    void TCPServer::Run(){
+        while( (this->sock_count = accept(this->socket_ret, (struct sockaddr *)&this->caddr, (socklen_t *)&this->saddr_size)) ){
+            new std::thread( TCPServer::Handler, this->sock_count );
+        }
     }
 
     void TCPServer::Stop(){
