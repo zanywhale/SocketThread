d57a4931bc29b581b181838ab3f50981c126c8b7
zanywhale <zanywhale@gmail.com>
2018-04-11 10:02:35 +0900

    Update: add Handler() & thread()

--- src/TCPServer.cc
+++ src/TCPServer.cc
@@ -11,7 +11,7 @@ namespace TCPServer{
                              
     void TCPServer::Setup(){
         this->socket_ret = socket(AF_INET, SOCK_STREAM, 0);
-        if(this->socket_ret == -1)
+        if(this->socket_ret == INVALID_SOCKET)
             std::cout << "Failed: can't create socket" << std::endl;
 
         this->saddr = {};
@@ -20,7 +20,7 @@ namespace TCPServer{
         this->saddr.sin_port = htons(this->port);
 
         // bind
-        if( bind(this->socket_ret, (struct sockaddr *)&saddr, sizeof(saddr)) < 0){
+        if( bind(this->socket_ret, (struct sockaddr *)&this->saddr, sizeof(saddr)) < 0){
             std::cerr<< "Failed: bind error" << std::endl;
             exit(1);
         }
@@ -30,13 +30,16 @@ namespace TCPServer{
 
     void TCPServer::Run(){
         while( (this->sock_count = accept(this->socket_ret, (struct sockaddr *)&this->caddr, (socklen_t *)&this->saddr_size)) ){
-            // thread, handler
-            // 
+            new std::thread(TCPServer::Handler, sock_count);
         }
     }
 
-    void TCPServer::Stop(){
+    void TCPServer::Handler(){
         
+    }
+
+    void TCPServer::Stop(){
+        close(this->socket_ret);
     }
 }
 
