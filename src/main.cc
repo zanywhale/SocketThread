ddb873b347e02e1e368dcade5587f21f3d6fc29b
zanywhale <zanywhale@gmail.com>
2018-04-11 10:56:55 +0900

    Feat: Add check condition

--- src/main.cc
+++ src/main.cc
@@ -2,5 +2,14 @@
 
 int main(int argc, char *argv[])
 {
+    int port = 0;
+    if(argc == 2){
+        port = atoi((char *)argv[1]);
+    }
+    else{
+        std::cout << argv[0] << " [PORT NUMBER]" << std::endl;
+        exit(0);
+    }
+
     return 0;
 }
