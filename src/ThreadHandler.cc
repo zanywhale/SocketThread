a4fa255d456f69168beca071c4732180a6045667
zanywhale <zanywhale@gmail.com>
2018-04-18 16:47:39 +0900

    feat: Add vector join

--- src/ThreadHandler.cc
+++ src/ThreadHandler.cc
@@ -8,13 +8,14 @@ namespace ThreadHandler{
 
     int ThreadHandler::Handlering(){
         while(true){
-            vec_mutex.lock();/*
-            for( this->iter = vec_thread->begin(); this->iter != vec_thread->end(); iter++ ){
-                if( !iter->joinable() ){
-                    iter->detach();
+            vec_mutex.lock();
+            for( unsigned int i = 0; i < vec_thread.size(); ++i ){
+                if( vec_thread[i].joinable() ){
+                    std::cout << "Join..." << vec_thread[i].get_id()<< std::endl; 
+                    vec_thread[i].join();
+                    // pop vector
                 }
-            }*/
-            std::cout << "hello" <<std::endl;
+            }
             vec_mutex.unlock();
             std::this_thread::sleep_for(std::chrono::milliseconds(10000));
         }
