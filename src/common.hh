54b58b8cdf4a17c7b316af12c9aa4b739c59181a
zanywhale <zanywhale@gmail.com>
2018-04-18 16:00:08 +0900

    feat: Add header and change vector

--- src/common.hh
+++ src/common.hh
@@ -3,12 +3,13 @@
 #include <unistd.h>
 #include <cstring>
 #include <mutex>
+#include <chrono>
 
 #include <vector>
 
 #include <sys/socket.h>
 #include <arpa/inet.h> // inet_addr
 
-extern std::vector<std::thread *> vec_thread;
+extern std::vector<std::thread> *vec_thread;
 extern std::mutex vec_mutex;
 
