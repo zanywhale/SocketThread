323047fef65f473fa1f601d30f5e0ad494fb1644
zanywhale <zanywhale@gmail.com>
2018-04-21 11:09:32 +0900

    feat: Change vector to list
    Change vector to list because of list characteristic.
    A vector can use this thread struct plan.

--- src/ThreadHandler.cc
+++ src/ThreadHandler.cc
@@ -8,15 +8,15 @@ namespace ThreadHandler{
 
     int ThreadHandler::Handlering(){
         while(true){
-            vec_mutex.lock();
-            for( unsigned int i = 0; i < vec_thread.size(); ++i ){
-                if( vec_thread[i].joinable() ){
-                    std::cout << "Join..." << vec_thread[i].get_id()<< std::endl; 
-                    vec_thread[i].join();
+            list_mutex.lock();
+            for( unsigned int i = 0; i < list_thread.size(); ++i ){
+                /*if( list_thread[i].joinable() ){
+                    std::cout << "Join..." << list_thread[i].get_id()<< std::endl; 
+                    list_thread[i].join();
                     // pop vector
-                }
+                }*/
             }
-            vec_mutex.unlock();
+            list_mutex.unlock();
             std::this_thread::sleep_for(std::chrono::milliseconds(10000));
         }
     }
