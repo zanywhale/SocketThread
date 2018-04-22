edb93ca1d937f54889c2e547cddeb92c696e9d9f
zanywhale <zanywhale@gmail.com>
2018-04-23 00:52:28 +0900

    feat: Update thread list handling
    Thread list handling was not perfect.
    But not now!
    list iterator is cause. Erase and iter++ so it causes error.
    So change some code for iterator reference error.

--- src/ThreadHandler.cc
+++ src/ThreadHandler.cc
@@ -9,11 +9,15 @@ namespace ThreadHandler{
     int ThreadHandler::Handlering(){
         while(true){
             list_mutex.lock();
-            for( this->iter = list_thread.begin() ; this->iter != list_thread.end() ; iter++ ){
+            for( this->iter = list_thread.begin() ; this->iter != list_thread.end() ;){
+                std::cout << this->iter->get_id() << std::endl;
                 if( this->iter->joinable() ){
-                    std::cout << "Join..." << this->iter->get_id()<< std::endl; 
-                    this->iter->join();
-                    // list_thread.erase(this->iter);
+                    std::cout << "Join..." << this->iter->get_id()<< std::endl;
+                    iter->detach();
+                    std::list<std::thread>::iterator tmp_iter = this->iter;
+                    tmp_iter++;
+                    list_thread.erase(iter);
+                    this->iter=tmp_iter;
                 }
             }
             list_mutex.unlock();
