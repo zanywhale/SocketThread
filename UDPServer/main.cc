18dd5a7d89fc2b1a5fe3218633de55af503dd9e7
zanywhale <zanywhale@gmail.com>
2018-04-24 08:20:17 +0900

    feat: Add new sources

---
+++ UDPServer/main.cc
@@ -0,0 +1,25 @@
+
+#include "common.hh"
+#include "UDPServer.hh"
+
+int main(int argc, char *argv[])
+{
+    int port = 0;
+    if(argc == 2){
+        port = atoi((char *)argv[1]);
+    }
+    else{
+        std::cout << argv[0] << " [PORT NUMBER]" << std::endl;
+        exit(0);
+    }
+
+    UDPServer::UDPServer server1(port);
+    UDPServer::UDPServer server2(port+1);
+    std::thread t1(&UDPServer::UDPServer::Run, server1);
+    std::thread t2(&UDPServer::UDPServer::Run, server2);
+    t1.join();
+    t2.join();
+
+    return 0;
+}
+
