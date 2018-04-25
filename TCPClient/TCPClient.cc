03abca4a1bfc9b8a245ffe47d0a86758e3713aa8
zanywhale <zanywhale@gmail.com>
2018-04-25 15:30:14 +0900

    feat: Update run method

--- TCPClient/TCPClient.cc
+++ TCPClient/TCPClient.cc
@@ -5,11 +5,14 @@ namespace TCPClient{
     TCPClient::TCPClient(std::string ipaddr, int port){
         this->port = port;
         this->ipaddr = ipaddr;
+        Setup();
+        Run(this->sock);
     }
 
     TCPClient::~TCPClient(){}
 
     void TCPClient::Setup(){
+        this->sock = socket(AF_INET, SOCK_STREAM, 0);
         this->saddr = {};
         this->saddr.sin_family = AF_INET;
         inet_pton(AF_INET, this->ipaddr.c_str(), &saddr.sin_addr);
