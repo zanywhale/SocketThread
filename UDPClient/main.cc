c390f3daf704da9b800f05aae804d0361939152a
zanywhale <zanywhale@gmail.com>
2018-04-25 15:14:41 +0900

    feat: Update argv check routine

---
+++ UDPClient/main.cc
@@ -0,0 +1,18 @@
+
+#include "common.hh"
+#include "UDPClient.hh"
+
+int main(int argc, char *argv[])
+{
+    int port = 0;
+    if(argc == 3){
+        port = atoi((char *)argv[2]);
+    }
+    else{
+        std::cout << argv[0] << " [IPADDR]" << " [PORT NUMBER]" << std::endl;
+        exit(0);
+    }
+
+    return 0;
+}
+
