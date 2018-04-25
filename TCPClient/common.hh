e01b2468a1b613b629df63aea0b7046af8ebdfc2
zanywhale <zanywhale@gmail.com>
2018-04-25 15:10:55 +0900

    feat: init

---
+++ TCPClient/common.hh
@@ -0,0 +1,26 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+#include <cstring>
+#include <mutex>
+#include <chrono>
+
+#include <vector>
+#include <list>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h> // inet_addr
+
+extern std::vector<std::thread> vec_thread;
+extern std::list<std::thread> list_thread;
+extern std::vector<std::string> vec_chat;
+extern std::mutex list_mutex;
+extern std::mutex vec_mutex;
+extern std::mutex chat_mutex;
+
+#endif // OOMMON_H
+
